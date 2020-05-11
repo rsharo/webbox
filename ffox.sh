89b8334f7fa86432104e821a0cba01a656dc3771
rsharo <rsharo@users.noreply.github.com>
2020-05-11 19:13:50 -0400

    Fixed config file-based ssh invocation in ffox.sh
    Turns out the -F argument inhibits loading /etc/ssh/ssh_config, which was setting ForwardX11Trusted yes
    So now I explicitly set ForwardX11Trusted in webbox-ssh-config/config and it works.

--- ffox.sh
+++ ffox.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-cd webbox-ssh-config
-exec ssh -X -p 2022 -i ./id_webbox guest@localhost firefox --no-remote
+#
+# Resolve script location to the DIR variable, following symlinks
+SOURCE="${BASH_SOURCE[0]}"
+while [[ -h "$SOURCE" ]]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
-#exec ssh -F config webbox firefox
+
+cd "${DIR}/webbox-ssh-config"
+exec ssh -F config webbox firefox
+#exec ssh -X -p 2022 -i ../id_webbox guest@localhost firefox --no-remote
